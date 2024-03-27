0903535dd27882ba53c65169357f31506a3aae31
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-03-27 15:27:18 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -3,3 +3,6 @@ create table bb(c1 number(3), c2 char(5), constraint myPK primary key (c1));
 alter table qq add constraint range check(c1 between 10 and 50);
 drop table qq;
 drop table bb;
+alter table qq add c3 number(5);
+alter table qq rename column c1 to c5;
+alter table qq modify c2 char(5);
